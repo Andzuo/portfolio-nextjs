e7fde98d96b7b655f59500360a4c39df6f8bbedb
AndersonVoeja <am@voeja.us>
2025-01-19 11:05:47 -0300

    feat: add navigation functionality to HeroSection component

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -2,14 +2,21 @@ import { ArrowRight } from "lucide-react";
 import React from "react";
 import { Button } from "../ui/button";
 import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 const HeroSection = () => {
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push("#about");
+  };
+
   return (
     <section
       id="home"
       className="relative min-h-screen flex items-center justify-center p-8"
     >
-      <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,#5900ff15,transparent_50%)]" />
+      <div className=" inset-0 bg-[radial-gradient(circle_at_center,#5900ff15,transparent_50%)]" />
       <div className="max-w-6xl mx-auto text-center space-y-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
@@ -23,15 +30,17 @@ const HeroSection = () => {
               Desenvolvedor Web <br /> Full Stack
             </span>
           </h1>
-          <p className="text-xl md:text-2xl text-gray-400 max-w-2xl mx-auto"></p>
         </motion.div>
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.5 }}
-          className="flex flex-wrap justify-center gap-4 mt-12"
+          className="flex flex-wrap justify-center gap-4 cursor-pointer"
         >
-          <Button className="bg-purple-600 mt-10 cursor-pointer hover:bg-purple-700 text-lg h-12 px-8">
+          <Button
+            onClick={handleNavigate}
+            className="bg-purple-600 mt-10 cursor-pointer hover:bg-purple-700 text-lg h-12 px-8"
+          >
             Iniciar Projeto
             <ArrowRight className="ml-2 h-5 w-5" />
           </Button>
