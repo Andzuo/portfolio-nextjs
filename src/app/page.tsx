d0bb2316ac7c7df6a90fd23cacbf58a066d5cba4
AndersonVoeja <am@voeja.us>
2025-01-19 11:05:31 -0300

    feat: integrate Footer component into the main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-// import { motion } from "framer-motion";
-// import { Button } from "@/components/ui/button";
-// import { Github, Twitch, Twitter } from "lucide-react";
-// import Image from "next/image";
 import HeroSection from "@/components/hero-section";
 import AboutSection from "@/components/about";
 import ProjectsSection from "@/components/projects";
 import FeedBacks from "@/components/feedBack";
+import Footer from "@/components/Footer";
 
 export default function Portfolio() {
   return (
@@ -18,24 +15,7 @@ export default function Portfolio() {
         <AboutSection />
         <ProjectsSection />
         <FeedBacks />
-        {/* <footer className="py-12 px-8 border-t border-white/10">
-          <div className="max-w-6xl mx-auto flex flex-wrap justify-between items-center gap-8">
-            <div className="flex items-center gap-4">
-              <Button variant="ghost" size="icon">
-                <Github className="h-6 w-6" />
-              </Button>
-              <Button variant="ghost" size="icon">
-                <Twitter className="h-6 w-6" />
-              </Button>
-              <Button variant="ghost" size="icon">
-                <Twitch className="h-6 w-6" />
-              </Button>
-            </div>
-            <p className="text-gray-400">
-              © 2024 Seu Nome. Todos os direitos reservados.
-            </p>
-          </div>
-        </footer> */}
+        <Footer />
       </div>
     </div>
   );
